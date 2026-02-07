a4a9c32747322378d26233ee3a63c7380a5b0fb1
SKSaniyaBegum <sksaniyabegum06@gmail.com>
2026-02-07 14:02:08 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,11 @@
+public class unary {
+    public static void main(String[] args) {
+        int a = 90;
+        int b = 78;
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(b--);
+        System.out.println(b++);
+    }
+
+}
