7f977071a8a9046ab779e000eb205795979b2f2a
SKSaniyaBegum <sksaniyabegum06@gmail.com>
2026-02-07 14:34:08 +0530

    Add files via upload

---
+++ sumof.java
@@ -0,0 +1,10 @@
+public class sumof {
+    public static void main(String[] args) {
+        int m, n;
+        m = 89;
+        n = 56;
+        int sum;
+        sum = (m + n);
+        System.out.println(sum);
+    }
+}
