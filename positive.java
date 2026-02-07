d06a42925c94293bea55441a636f52cf39583f22
SKSaniyaBegum <sksaniyabegum06@gmail.com>
2026-02-07 14:43:01 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,12 @@
+public class positive {
+    public static void main(String[] args) {
+        int number=90;
+        if (number > 0) {
+            System.out.println(number + " is Positive.");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+    }
+}
